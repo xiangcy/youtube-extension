99089d8a6e008c005184b9feff507fcf6a234064
Chongyuan <xiangcy@mit.edu>
2013-10-05 16:39:07 -0400

    load storage

--- js/addsong.js
+++ js/addsong.js
@@ -1,12 +1,15 @@
-//$(function(){
-	chrome.tabs.query({active:true, currentWindow:true},function(tab) {
-		var tabID=tab[0].id
+// //$(function(){
+// 	chrome.tabs.query({active:true, currentWindow:true},function(tab) {
+// 		var tabID=tab[0].id
 
-	  	alert(chrome.tabs.get(tabID));
-    });
-//});
+// 	  	alert(chrome.tabs.get(tabID));
+//     });
+// //});
 
 $(function(){
   var song_list_div = $('#songTable');
-  for 
+  var key_list = chrome.extension.getBackgroundPage().song_storage.get_list();
+  for (var i = 0; i < key_list.length; i++) {
+    song_list_div.append("<tr><td>" + key_list[i] + "</td></tr>");
+  }
 });
