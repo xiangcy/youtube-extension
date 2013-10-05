6ca0d23d1f3d26c548724af00e7dd0d39948918b
Xinyue Deng <dxy0420@mit.edu>
2013-10-05 16:37:03 -0400

    nothing

--- js/addsong.js
+++ js/addsong.js
@@ -1,7 +1,14 @@
-//$(function(){
+$(function(){
 	chrome.tabs.query({active:true, currentWindow:true},function(tab) {
-		var tabID=tab[0].id
+	var tabID=tab[0].id
+	chrome.tabs.get(tabID, function(tab){
+		var url=tab.url;
+	});
+});
+alert(url);
+$("#addlist").click(function(){
+	alert(url);
+});
+});
+	
 
-	  	alert(chrome.tabs.get(tabID));
-    });
-//});
